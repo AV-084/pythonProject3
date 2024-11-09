f0e02901540f51aedaf94780dac0e73f1e6e920c
AV-084 <vorotnikof1109@gmail.com>
2024-11-09 20:25:30 +0300

    Code 3-20

--- list.py
+++ list.py
@@ -13,3 +13,19 @@ def test2(*args, **kwargs):
 test2 ([1, 2], 3, 'Anna', 10, {'Vasia': 45, 'Petia': 46}) #-> [1, 2] 3 Anna 10 {'Vasia': 45, 'Petia': 46}
 
 test2 (*[1, 2], 3, 'Anna', 10, *{'Vasia': 45, 'Petia': 46}) #-> 1 2 3 Anna 10 Vasia Petia
+
+'Конкантинация (склеивание)'
+list_1 = [1, 2, 3]
+list_2 = [4, 5, 6]
+result = str(list_1[0]) + str(list_2[0])
+print(result) # -> 14
+
+'Добавление вложенных списков'
+matrix = []
+matrix.append([])
+print(matrix) # ->[[]]
+matrix.append([])
+print(matrix) # ->[[], []]
+element = 5
+matrix[0].append(element)
+print(matrix) # ->[[5], []]
